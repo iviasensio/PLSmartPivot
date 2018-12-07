8b0a430e4b9a003fca166c792a133cf120126bc5
Alexander Szmidt <alexander.szmidt@gmail.com>
2018-12-07 16:58:57 +0100

    Add dummy .spec to please CircleCI

---
+++ src/haiku.spec.js
@@ -0,0 +1,9 @@
+/* https://randomhaiku.com */
+
+describe('behind the money', () => {
+  describe('Canada and Panda work.', () => {
+    it('Tiger starts blowing.', () => {
+      expect(true).toBeTruthy();
+    });
+  });
+});
