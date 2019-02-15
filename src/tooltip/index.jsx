906a11c6b4c75baa108101875602c53a72c3dbe1
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2019-02-15 08:56:26 +0100

    tooltip component cleaned

--- src/tooltip/index.jsx
+++ src/tooltip/index.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-const getPos = () => {
-  console.log(window.pageXOffset);
-  return {
-    left: window.pageXOffset,
-    top: window.pageYOffset
-  };
-
-};
 
 const Tooltip = ({ data, xPosition, yPosition }) => (
   <div
-    style={{ "left": `${xPosition - 20}px`,
-      "top": `${yPosition - 75}px` }} className="tooltip-wrapper"
+    className="tooltip-wrapper"
+    style={{ 'left': `${xPosition - 20}px`,
+      'top': `${yPosition - 75}px` }}
   >
     <p>
       {data}
