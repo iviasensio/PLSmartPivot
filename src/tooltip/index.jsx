fe4b5a72ec37f13b84abae7eb9888357470e262e
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2019-02-15 09:56:32 +0100

    handling tooltip logic within it

--- src/tooltip/index.jsx
+++ src/tooltip/index.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Tooltip = ({ data, xPosition, yPosition }) => (
-  <div
-    className="tooltip-wrapper"
-    style={{ 'left': `${xPosition - 20}px`,
-      'top': `${yPosition - 75}px` }}
-  >
-    <p>
-      {data}
-    </p>
-  </div>
-);
+const Tooltip = ({ children }) => {
+  const xPosition = event.clientX;
+  const yPosition = event.clientY;
+  return (
+    <div
+      className="tooltip-wrapper"
+      style={{ 'left': `${xPosition - 20}px`,
+        'top': `${yPosition - 75}px` }}
+    >
+      <p>
+        {children}
+      </p>
+    </div>
+  );
+};
 Tooltip.propTypes = {
-  data: PropTypes.string.isRequired,
-  xPosition: PropTypes.number,
-  yPosition: PropTypes.number
+  children: PropTypes.string.isRequired
 };
 export default Tooltip;
