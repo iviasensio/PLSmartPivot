979c036b49b90ce4cdbb0675126b4bc20f7c3b0e
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2019-02-22 12:44:44 +0100

    settings file reverted

--- settings.js
+++ settings.js
@@ -4,10 +4,10 @@ const packageJSON = require('./package.json');
 const defaultBuildDestination = path.resolve("./build");
 
 module.exports = {
-  buildDestination: "C:\\Users\\Ahmed\\Documents\\Qlik\\Sense\\Extensions\\qlik-smart-pivot",
+  buildDestination: process.env.BUILD_PATH || defaultBuildDestination,
   mode: process.env.NODE_ENV || 'development',
   name: packageJSON.name,
   version: process.env.VERSION || 'local-dev',
   url: process.env.BUILD_URL || defaultBuildDestination,
-  port: process.env.PORT || 8090
+  port: process.env.PORT || 8085
 };
