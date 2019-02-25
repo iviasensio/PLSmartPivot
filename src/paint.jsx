7107f485be1c2ab6f3c779f8fd5de8a91d7372e5
Balazs Gobel <balazs.gobel@tretton37.com>
2019-02-25 12:55:48 +0100

    resolve additional merge conflict: removed obsolete code

--- src/paint.jsx
+++ src/paint.jsx
@@ -20,20 +20,4 @@ export default async function paint ($element, layout, component) {
   );
 
   ReactDOM.render(jsx, $element[0]);
-
-  // TODO: skipped the following as they weren't blockers for letting react handle rendering,
-  // they are however the only reason we still depend on jQuery and should be removed as part of unnecessary dependencies issue
-  $(`[tid="${layout.qInfo.qId}"] .data-table .row-wrapper`).on('scroll', function () {
-    $(`[tid="${layout.qInfo.qId}"] .kpi-table .row-wrapper`).scrollTop($(this).scrollTop());
-  });
-
-  // freeze first column
-  $(`[tid="${layout.qInfo.qId}"] .qv-object-content-container`).on('scroll', (t) => {
-    $(`[tid="${layout.qInfo.qId}"] .kpi-table`).css('left', `${Math.round(t.target.scrollLeft)}px`);
-  });
-
-  // TODO: excel export is broken in most browsers, fixing it has an issue of it's own (leaving it disabled for now)
-  // import { enableExcelExport } from './excel-export';
-  // enableExcelExport(layout, html);
-
 }
